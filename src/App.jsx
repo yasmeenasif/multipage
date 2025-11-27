cfa0b1c04216dce5d47f12dc7b09c7791baacda8
yasmeenasif <zaheeryasmeen05@gmail.com>
2025-11-27 15:16:00 +0530

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import {HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
